f276bb524c450759660cf93c748a3c39f76370e9
aswini <aswini5427@gmail.com>
2023-12-15 07:46:58 +0000

    adde

--- src/com/LokedMe/BusinessOperations.java
+++ src/com/LokedMe/BusinessOperations.java
@@ -2,4 +2,28 @@ package com.LokedMe;
 
 public class BusinessOperations {
 
-}
+	public static void listFiles() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void createFile(String fileCreate) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void deleteFile(String fileDelete) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void searchFile(String fileSearch) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+		
+	}
+
+
